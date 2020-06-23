211b61853a02026ca038b17280cf2e104b0c7d4a
jbdesbas <jb.desbas@gmail.com>
2020-06-23 09:24:49 +0200

    custom query

---
+++ backend/customs_query/annual_stats.py
@@ -0,0 +1,22 @@
+from . import CustomQuery
+
+class MyQuery(CustomQuery) :
+    def __init__(self):
+        super().__init__()
+        self.sql_text = """
+          SELECT
+	        "year"::int,
+	        obs_created::int,obs_observed::int,new_sp_created::int,new_sp_observed::int,
+	        (sum(obs_created ) OVER (ORDER BY "year"))::int AS obs_created_cumul,
+	        (sum(obs_observed ) OVER (ORDER BY "year"))::int AS obs_observed_cumul,
+	        (sum(new_sp_created ) OVER (ORDER BY "year"))::int AS new_sp_created_cumul,
+	        (sum(new_sp_observed ) OVER (ORDER BY "year"))::int AS new_sp_created_cumul
+        FROM shared.annual_stats 
+        ORDER BY "year";
+        """
+        
+        self.help="""
+            Statistiques annuelles de la base
+        """
+    
+_qr = MyQuery()
---
+++ backend/customs_query/pheno.py
@@ -0,0 +1,33 @@
+from . import CustomQuery
+
+class MyQuery(CustomQuery) :
+    def __init__(self):
+        super().__init__()
+        #ajouter un "mode hiver", qui place le début de l'année le 1er juillet
+        self.sql_text = """
+           SELECT 
+                tx.cd_nom,
+                tx.lb_nom,
+                min(EXTRACT(DOY FROM date_min)) AS "min",
+            	percentile_cont(0.01) WITHIN GROUP ( ORDER BY (  EXTRACT(DOY FROM date_min) ) ) AS "low",
+		        percentile_cont(0.25) WITHIN GROUP ( ORDER BY (EXTRACT(DOY FROM date_min ) ) ) AS q1,
+		        percentile_cont(0.5) WITHIN GROUP ( ORDER BY (EXTRACT(DOY FROM date_min  ) ) ) AS q2,
+                percentile_cont(0.75) WITHIN GROUP ( ORDER BY (EXTRACT(DOY FROM date_min ) ) ) AS q3,
+                percentile_cont(0.99) WITHIN GROUP ( ORDER BY (EXTRACT(DOY FROM date_min ) ) ) AS "high",
+                max(EXTRACT(DOY FROM date_min)) AS "max",
+                array_agg(DISTINCT EXTRACT(DOY FROM date_min::date)::int ORDER BY EXTRACT(DOY FROM date_min::date)::int ) as doy
+            FROM gn_synthese.synthese_usable s
+            JOIN taxonomie.taxref tx ON tx.cd_nom = taxonomie.find_cdref_sp(s.cd_nom)
+            WHERE 
+                tx.cd_nom IN (SELECT cd_nom FROM taxonomie.find_all_taxons_children((:cd_nom)::int))
+                AND date_min=date_max AND date_min >=now()-'10 years'::interval
+            GROUP BY tx.cd_nom
+           ORDER BY percentile_cont(0.5) WITHIN GROUP ( ORDER BY (EXTRACT(DOY FROM date_min  ) ) )
+        LIMIT 500; """
+        
+        self.args_default = {'cd_nom':186206} #Mammalia
+        self.help="""
+            Renvoi la phénologie des espèces pour dessiner des boites à moustaches [0.05,0.25,0.5,0.75,0.95]
+        """
+    
+_qr = MyQuery()
